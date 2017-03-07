13a30ab7e5f364aabe58f65ccb393ab8f89083f9
Riku Hashiyada <rhashiya@ucsd.edu>
2017-03-07 00:05:43 -0800

    Initial Commit

---
+++ SaveTheKittens/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SaveTheKittens
+//
+//  Created by Riku Hashiyada on 3/7/17.
+//  Copyright © 2017 Riku Hashiyada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
